5ce45f7c909523911c3507ba2e71ddfa105e854c
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-26 06:54:46 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,11 +2,11 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => {},
-        :age => []
+        :name => {"Lord Montague"},
+        :age => {"53"}
         },
       :matriarch => {
-        #your key/value pairs here
+        
         },
       :hero => {
         #your key/value pairs here
